b28ce619bfba9d71c685547c4b0a8603484a4995
jaemin <als982001@naver.com>
2025-06-22 17:33:08 +0900

    Search Page

--- app/features/products/pages/search-page.tsx
+++ app/features/products/pages/search-page.tsx
@@ -1,8 +1,71 @@
-export default function SearchPage() {
+import { Form } from "react-router";
+import { z } from "zod";
+
+import type { Route } from "./+types/search-page";
+
+import { Hero } from "~/common/components/hero";
+import { Input } from "~/common/components/ui/input";
+import { Button } from "~/common/components/ui/button";
+import ProductPagination from "~/common/components/product-pagination";
+
+import { ProductCard } from "../components/product-card";
+
+export const meat: Route.MetaFunction = () => {
+  return [
+    { title: "Search Products | wemake" },
+    { name: "description", content: "Search for products" },
+  ];
+};
+
+const paramsSchema = z.object({
+  query: z.string().optional().default(""),
+  page: z.coerce.number().optional().default(1),
+});
+
+export function loader({ request, params }: Route.LoaderArgs) {
+  console.log({ params, request });
+
+  const url = new URL(request.url);
+
+  const { success, data: parsedData } = paramsSchema.safeParse(
+    Object.fromEntries(url.searchParams)
+  );
+
+  if (!success) {
+    throw new Error("Invalid params");
+  }
+}
+
+export default function SearchPage({ loaderData }: Route.ComponentProps) {
   return (
-    <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold mb-6">Search Results</h1>
-      {/* Add search results */}
+    <div className="space-y-10">
+      <Hero
+        title="Search"
+        subtitle="Search for products by title or description"
+      />
+      <Form className="flex justify-center h-14 max-w-screen-sm items-center gap-2 mx-auto">
+        <Input
+          name="query"
+          placeholder="Search for products"
+          className="text-lg"
+        />
+        <Button type="submit">Search</Button>
+      </Form>
+      <div className="space-y-5 w-full max-w-screen-md mx-auto">
+        Add commentMore actions
+        {Array.from({ length: 11 }).map((_, index) => (
+          <ProductCard
+            key={`productId-${index}`}
+            id={`productId-${index}`}
+            name="Product Name"
+            description="Product Description"
+            commentsCount={12}
+            viewsCount={12}
+            votesCount={120}
+          />
+        ))}
+      </div>
+      <ProductPagination totalPages={10} />
     </div>
   );
 }
