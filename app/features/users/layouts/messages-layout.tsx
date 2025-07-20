ff17eb46225800945e76b65c4591678b324ee8ed
jaemin <als982001@naver.com>
2025-07-20 17:42:26 +0900

    messages page

---
+++ app/features/users/layouts/messages-layout.tsx
@@ -0,0 +1,38 @@
+import { Outlet } from "react-router";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarMenu,
+  SidebarProvider,
+} from "~/common/components/ui/sidebar";
+
+import { MessageCard } from "../components/message-card";
+
+export default function MessagesLayout() {
+  return (
+    <SidebarProvider>
+      <Sidebar>
+        <SidebarContent>
+          <SidebarGroup>
+            <SidebarMenu>
+              {Array.from({ length: 20 }).map((_, index) => (
+                <MessageCard
+                  key={index}
+                  id={index.toString()}
+                  name={`User ${index}`}
+                  lastMessage={`Last message ${index}`}
+                  avatarUrl={`https://github.com/serranoarevalo.png`}
+                />
+              ))}
+            </SidebarMenu>
+          </SidebarGroup>
+        </SidebarContent>
+      </Sidebar>
+      <div className=" h-full flex-1">
+        <Outlet />
+      </div>
+    </SidebarProvider>
+  );
+}
